3bf70e48e20a265603da65ad9263088375f00c50
DEROIDE Antoine <147041467+AntoineDeroide@users.noreply.github.com>
2025-11-04 08:38:07 +0100

    1.2

--- Maths-AsciiDonut/Maths-AsciiDonut/main.cpp
+++ Maths-AsciiDonut/Maths-AsciiDonut/main.cpp
@@ -6,7 +6,15 @@ int main(int argc, char** argv)
 {
 	std::cout << "Hello World !\n";
 
+// 1.2
+	std::cout << "Hello...\n";
+	std::cout << "\033[2J"; // console clear
+	std::cout << "...hello?\n";
 
+	std::cout << "\033[H"; // Move cursor to home position
+
+	std::cout << "\033[?25l"; // Make cursor invisible
+	std::cout << "\033[?25h"; // Make cursor visible
 
 
 }
