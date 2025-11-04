f4408763c953ef34b1537226f425455617aa878d
DEROIDE Antoine <147041467+AntoineDeroide@users.noreply.github.com>
2025-11-04 08:50:50 +0100

    1.3

--- Maths-AsciiDonut/Maths-AsciiDonut/main.cpp
+++ Maths-AsciiDonut/Maths-AsciiDonut/main.cpp
@@ -4,17 +4,35 @@
 // 1.1
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World !\n";
+	//std::cout << "Hello World !\n";
 
 // 1.2
-	std::cout << "Hello...\n";
-	std::cout << "\033[2J"; // console clear
-	std::cout << "...hello?\n";
+	//std::cout << "Hello...\n";
+	//std::cout << "\033[2J"; // console clear
+	//std::cout << "...hello?\n";
 
-	std::cout << "\033[H"; // Move cursor to home position
+	//std::cout << "\033[H"; // Move cursor to home position
 
-	std::cout << "\033[?25l"; // Make cursor invisible
-	std::cout << "\033[?25h"; // Make cursor visible
+	//std::cout << "\033[?25l"; // Make cursor invisible
+	//std::cout << "\033[?25h"; // Make cursor visible
+
+	//////////////////////////////////////////////////////////////////////
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);					//
+	DWORD mode;															//
+	GetConsoleMode(hConsole, &mode);									//
+	SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);//
+	//////////////////////////////////////////////////////////////////////
+
+// 1.3
+	std::cout << "\033[?25l"; // Set cursor to invisible
+	std::cout << "\033[2J"; // Clear console
+	std::cout << "\033[H"; // cursor to home pos
+	std::cout << "Hello World !\n\n";
+
+
+
+
+
 
 
 }
