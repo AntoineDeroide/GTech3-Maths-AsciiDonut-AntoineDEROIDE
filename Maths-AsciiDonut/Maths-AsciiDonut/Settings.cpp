1964426f9d90341f1f0f7de40498f40685caaa04
DEROIDE Antoine <147041467+AntoineDeroide@users.noreply.github.com>
2025-11-25 09:00:18 +0100

    2.4
    Added parameters

--- Maths-AsciiDonut/Maths-AsciiDonut/Settings.cpp
+++ Maths-AsciiDonut/Maths-AsciiDonut/Settings.cpp
@@ -1,7 +1,12 @@
 #include "Settings.h"
 #include <iostream>
 
-Settings::Settings(int argc, char** argv) : m_width(100), m_height(20), m_resolution(32)
+Settings::Settings(int argc, char** argv) :
+	m_width(100),
+	m_height(20),
+	m_resolution(32),
+	m_screenPosition(3.33f),
+	m_meshPosition(5.0f)
 {
 	if (argc < 5)
 	{
@@ -11,18 +16,40 @@ Settings::Settings(int argc, char** argv) : m_width(100), m_height(20), m_resolu
 	for (int i = 1; i < argc; i++)
 	{
 		if (argv[i] == "-w" && (i + 1) <= argc)
-		{
+		{	// SCREEN WIDTH
 			m_width = atoi(argv[i + 1]);
 			i++;
 		}
-		if (argv[i] == "-w" && (i + 1) <= argc)
-		{
+		if (argv[i] == "-h" && (i + 1) <= argc)
+		{	// SCREEN HEIGHT
 			m_height = atoi(argv[i + 1]);
 			i++;
 		}
 		if (argv[i] == "-r" && (i + 1) <= argc)
-		{
+		{	// MESH RESOLUTION
 			m_resolution = atoi(argv[i + 1]);
+			i++;
+		}
+		if (argv[i] == "-s" && (i + 1) <= argc)
+		{	// SCREEN POSITION
+			m_screenPosition = atof(argv[i + 1]);
+			i++;
+		}
+		if (argv[i] == "-m" && (i + 1) <= argc)
+		{	// MESH DISTANCE FROM SCREEN
+			m_meshPosition = atof(argv[i + 1]);
+			i++;
+		}
+		
+		// DisplayedChar
+		if (argv[i] == "-b" && (i + 1) <= argc)
+		{	// BACKGROUND
+			m_displayParameters[0] = argv[i + 1][0];
+			i++;
+		}
+		if (argv[i] == "-p" && (i + 1) <= argc)
+		{	// MESH PIXELS
+			m_displayParameters[1] = argv[i + 1][0];
 			i++;
 		}
 	}
@@ -41,4 +68,14 @@ int Settings::GetHeight()
 int Settings::GetResolution()
 {
 	return m_resolution;
+}
+
+float Settings::GetScreenPosition()
+{
+	return m_screenPosition;
+}
+
+float Settings::GetMeshPosition()
+{
+	return m_meshPosition;
 }
--- Maths-AsciiDonut/Maths-AsciiDonut/Settings.h
+++ Maths-AsciiDonut/Maths-AsciiDonut/Settings.h
@@ -1,5 +1,11 @@
 #pragma once
 
+enum DisplayedChar : char
+{
+	BACKGROUND_CHAR = ' ',
+	PROJECTION_CHAR = 'X',
+};
+
 class Settings
 {
 public:
@@ -8,9 +14,17 @@ public:
 	int GetWidth();
 	int GetHeight();
 	int GetResolution();
+	int GetScreenPosition();
+	int GetMeshPosition();
 
 private:
-	int m_width, m_height, m_resolution;
+	int m_width;
+	int m_height;
+	int m_resolution;
+	float m_screenPosition;
+	float m_meshPosition;
+
+	char m_displayParameters[2]; // 0 == background ; 1 == mesh pixels
 
 	friend class Screen;
 };
